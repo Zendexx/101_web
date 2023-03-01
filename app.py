604e1a8aac4c9dd0c8814fc2b4ab0e0d5a1ad733
DUAL SHOCK <danielbryan62F@gmail.com>
2023-03-01 10:42:27 +0300

    added new changes

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask,render_template
+
+app=Flask (__name__)
+
+@app.route("/")
+def home_page():
+    return render_template("index.html")
+    
+@app.route("/about")
+def about():
+    return "This is the about page"
+
+@app.route("/home")
+def home():
+    return "<h1>this is home page</h1>"
+
